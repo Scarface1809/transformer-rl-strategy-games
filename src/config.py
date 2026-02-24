d695db665c491b97e7a1b04d9f3fe17373b1fcbc
Scarface1809 <joao.miguel.santos1809@gmail.com>
2026-02-24 23:28:55 +0000

    refactor: simplify config

--- src/config.py
+++ src/config.py
@@ -3,13 +3,7 @@ from dataclasses import dataclass, field
 # --- Environment ---
 @dataclass
 class EnvConfig:
-    board: str = "random"  # "random" or "hispania"
-    num_tiles: int = 25
-    num_nations: int = 4
-    initial_units: int = 10
-    max_turns: int = 20
-    fixed_map: bool = True
-    seed: str = None
+    preset: str = "hispania"  # board: "hispania"
 
 # --- Model ---
 @dataclass
@@ -20,14 +14,17 @@ class ModelConfig:
     n_layers: int = 2
 
 # --- Training ---
-# 100 Training - 10 Test ... Repeat
 @dataclass
 class TrainingConfig:
-    num_episodes: int = 2000
-    num_games: int = 100
+    num_games: int = 3000
     gamma: float = 0.99
     lr: float = 1e-3
-    device: str = None
+    debug: bool = True
+
+@dataclass
+class EvaluateConfig:
+    num_games: int = 10
+    frequency: int = 100  # Evaluate every *frequency* training games
     debug: bool = True
 
 @dataclass
@@ -35,3 +32,4 @@ class Config:
     env: EnvConfig = field(default_factory=EnvConfig)
     model: ModelConfig = field(default_factory=ModelConfig)
     training: TrainingConfig = field(default_factory=TrainingConfig)
+    evaluation: EvaluateConfig = field(default_factory=EvaluateConfig)
