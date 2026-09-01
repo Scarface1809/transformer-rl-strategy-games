b83647708d1f552967cd6ae6448ef9fa7439cbdf
Scarface1809 <joao.miguel.santos1809@gmail.com>
2026-09-01 12:22:00 +0100

    fix: update training configs

--- src/config.py
+++ src/config.py
@@ -19,17 +19,17 @@ class ModelConfig:
 @dataclass
 class TrainingConfig:
     # MCTS
-    mcts_sims: int = 1024             # More simulations = slower but better TODO: 1600 increase
+    mcts_sims: int = 128             # More simulations = slower but better TODO: 1600 increase
     mcts_c_puct: float = 1.0          # Exploration vs exploitation
     
     lr: float = 0.0003                # Learning rate TODO: Increase to 0.001
     value_coef: float = 1.0           # Weight of value loss (scalar to balance with policy loss)
     max_grad_norm: float = 1.0        # Gradient clipping threshold
     batch_size: int = 256             # Batch size for training TODO: Increase to 4096 this is becayuse buffersize is way biger so idk.\
-    num_train_epochs: int = 10        # How many epochs per buffer sample
-    buffer_size: int = 10000          # Max transitions in buffer (also used for plotting)
+    num_train_epochs: int = 5        # How many epochs per buffer sample
+    buffer_size: int = 50000          # Max transitions in buffer (also used for plotting)
     frequency_games: int = 100        # Train after every N self-play games #TODO Do 100 games or 200 not sure
-    epochs: int = 5000               # Total training epochs (games) # TODO: Less epoches maybe?
+    epochs: int = 3000               # Total training epochs (games) # TODO: Less epoches maybe?
     debug: bool = True                # Verbose logging
 
 # --- Evaluation ---
